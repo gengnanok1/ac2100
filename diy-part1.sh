ea9dc7b2cbef44f3f7c5cab6774d1a934f08d835
anwugos <65577106+anwugos@users.noreply.github.com>
2022-04-21 15:35:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+#=============================================================
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# 增加lean的ssr-plus
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+
+# 增加Lienol大神软件包 passwall及其依赖
+echo "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.default
+# 增加Kenzok8的软件库 包含passwall及其依赖
+#echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
+# echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
+# ./scripts/feeds update -a && ./scripts/feeds install -a
